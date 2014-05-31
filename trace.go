a29b9b41efde04fb6150e3a1b41a2307c96453c4
Anindya Chatterjee <anidotnet@gmail.com>
2014-05-31 13:35:11 +0530

    Work In Progess

--- trace.go
+++ trace.go
@@ -1,4 +1,4 @@
-package tracer
+package trace
 
 import (
 	"fmt"
@@ -6,7 +6,7 @@ import (
 	"runtime"
 	"os"
 	"path/filepath"
-	"io"
+	"runtime/debug"
 )
 
 // Use these configuration variables to set tracing parameters
@@ -17,32 +17,76 @@ var (
 	PRINT_TO_STDOUT		bool 	= true
 	// Flag to set the tracer to print in Stdout
 	PRINT_TO_FILE		bool 	= false
-	// Trace log file
+	// Trace log file path
 	TRACE_OUT_FILE		string
 	// Trace message format
 	TRACE_FORMAT		string	= "TRACE [%v] <%s> (%s:%d) - %v"
 )
 
-// Trace writes information about current source file, line number and input variable
-func Trace(variable interface {}) {
+// Writes information about current source file, line number and input variable
+func Write(variable interface {}) {
 	if ENABLE_TRACE {
 		msg := createTraceMessage(variable)
-		if PRINT_TO_STDOUT {
-			fmt.Println(msg)
-		}
+		trace(msg, false)
+	}
+}
 
-		if PRINT_TO_FILE {
-			if TRACE_OUT_FILE == "" {
-				return
-			} else {
-				file, _ := os.OpenFile(TRACE_OUT_FILE, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-				defer file.Close()
-				file.WriteString(msg + "\n")
+// Writes information about current source file, line number and input variable after
+// formatting it according to a format specifier
+func Writef(format string, variables ...interface{}) {
+	if ENABLE_TRACE {
+		msg := createTraceMessagef(format, variables...)
+		trace(msg, false)
+	}
+}
+
+// Checks for a condition; if the condition is false, writes
+// a trace message that shows the call stack.
+func Assert (condition bool) {
+	if ENABLE_TRACE {
+		if !condition {
+			msg := createTraceMessage("Assertion Failed!")
+			trace(msg, true)
+		}
+	}
+}
+
+// Checks for a condition; if the condition is false, writes a
+// specified message after formatting it according to a format specifier
+// that shows the call stack.
+func Assertf (format string, condition bool) {
+	if ENABLE_TRACE {
+		if !condition {
+			msg := createTraceMessagef(format, "Assertion Failed!")
+			trace(msg, true)
+		}
+	}
+}
+
+// Prints the message and stack trace if set to true
+func trace(msg string, printStack bool) {
+	if PRINT_TO_STDOUT {
+		fmt.Println(msg)
+		if printStack {
+			debug.PrintStack()
+		}
+	}
+
+	if PRINT_TO_FILE {
+		if TRACE_OUT_FILE == "" {
+			return
+		} else {
+			file, _ := os.OpenFile(TRACE_OUT_FILE, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+			defer file.Close()
+			file.WriteString(msg + "\n")
+			if printStack {
+				file.WriteString(string(debug.Stack()))
 			}
 		}
 	}
 }
 
+// Creates the trace message
 func createTraceMessage(variable interface {}) string {
 	pc, path, line, ok := runtime.Caller(2)
 	if ok {
@@ -56,37 +100,16 @@ func createTraceMessage(variable interface {}) string {
 	return ""
 }
 
-// Trace writes information about current source file, line number and input variable after
+// Creates the trace message after
 // formatting it according to a format specifier
-func Tracef(format string, variables ...interface{}) {
-	if ENABLE_TRACE {
-		msg := createTraceMessagef(format, variables)
-		if PRINT_TO_STDOUT {
-			fmt.Println(msg)
-		}
-
-		if PRINT_TO_FILE {
-			if TRACE_OUT_FILE == "" {
-				return
-			} else {
-				file, _ := os.OpenFile(TRACE_OUT_FILE, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-				defer file.Close()
-				file.WriteString(msg + "\n")
-			}
-		}
-	}
-}
-
-
-
 func createTraceMessagef(format string, variables ...interface{}) string {
-	pc, path, line, ok := runtime.Caller(1)
+	pc, path, line, ok := runtime.Caller(2)
 	if ok {
 		fun := runtime.FuncForPC(pc)
 		name := fun.Name()
 
 		sourceFile := filepath.Base(path)
-		umsg := fmt.Sprintf(format, variables)
+		umsg := fmt.Sprintf(format, variables...)
 		msg := fmt.Sprintf(TRACE_FORMAT, time.Now(), name, sourceFile, line, umsg)
 		return msg
 	}
--- trace_test.go
+++ trace_test.go
@@ -1,15 +1,90 @@
-package tracer
+package trace
 
-import "testing"
+import (
+	"testing"
+	"io/ioutil"
+	"fmt"
+	"runtime"
+	"os"
+	"strings"
+	"strconv"
+)
 
-func TestTrace(t *testing.T) {
+var tempFile *os.File
 
+// CAUTION!!!! - Line sensitive test - Line = 20
+func TestWrite(t *testing.T) {
+	init_trace()
+	defer close_trace()
+	someVariable := "write-test"
+	Write(someVariable)
+	verify(t, someVariable, 20, "trace_test.go")
 }
 
-func TestTracef(t *testing.T) {
-
+// CAUTION!!!! - Line sensitive test - Line = 29
+func TestWritef(t *testing.T) {
+	init_trace()
+	defer close_trace()
+	someVariable := "write-test"
+	Writef("format-test %s", someVariable)
+	finalmsg := fmt.Sprintf("format-test %s", someVariable)
+	verify(t, finalmsg, 29, "trace_test.go")
 }
 
-func setStdInOut() {
+// CAUTION!!!! - Line sensitive test - Line = 38
+func TestAssert(t *testing.T) {
+	init_trace()
+	defer close_trace()
+	Assert(2 == 3)
+	verify(t, "Assertion Failed!", 38, "trace_test.go")
+}
 
+// CAUTION!!!! - Line sensitive test - Line = 46
+func TestAssertf(t *testing.T) {
+	init_trace()
+	defer close_trace()
+	Assertf("False %v", 2 == 3)
+	verify(t, "False Assertion Failed!", 46, "trace_test.go")
+}
+
+func verify(t *testing.T, message string, line int, file string) {
+	msg, err := ioutil.ReadAll(tempFile)
+	if err != nil {
+		assert(t, false, 1)
+	}
+	if !strings.Contains(string(msg), message) {
+		assert(t, false, 1)
+	}
+
+	fileLine := file + ":" + strconv.Itoa(line)
+	if !strings.Contains(string(msg), fileLine) {
+		assert(t, false, 1)
+	}
+}
+
+func init_trace() {
+	ENABLE_TRACE = true
+	var err error
+	tempFile, err = ioutil.TempFile("", "trace_test")
+	if err != nil {
+		fmt.Println("Couldnot create temp file.")
+		runtime.Goexit()
+	}
+	TRACE_OUT_FILE = tempFile.Name()
+	PRINT_TO_FILE = true
+}
+
+func close_trace() {
+	tempFile.Close()
+	os.Remove(TRACE_OUT_FILE)
+	PRINT_TO_FILE = false
+	ENABLE_TRACE = false
+}
+
+func assert(t *testing.T, result bool, cd int) {
+	if !result {
+		_, file, line, _ := runtime.Caller(cd + 1)
+		t.Errorf("%s:%d", file, line)
+		t.FailNow()
+	}
 }
